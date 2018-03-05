4fae4b81f1c595271c4ba827860f9bec8b2aada0
Haytham akl <sc@dlmar.com>
2018-03-05 17:29:55 -0500

    Kick off

---
+++ server.js
@@ -0,0 +1,48 @@
+// Require our dependencies
+const express = require('express'),
+  exphbs = require('express-handlebars'),
+  http = require('http'),
+  mongoose = require('mongoose'),
+  twitter = require('ntwitter'),
+  routes = require('./routes'),
+  config = require('./config'),
+  streamHandler = require('./utils/streamHandler');
+  Config = require('./config');
+
+const app = express();
+const port = process.env.PORT || 1666;
+
+// Set handlebars as the templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Disable etag headers on responses
+app.disable('etag');
+
+// Connect to our mongo database
+mongoose.connect('mongodb://localhost/react-tweets');
+
+// Create a new ntwitter instance
+var twit = new twitter(config.twitter);
+
+// Index Route
+app.get('/', routes.index);
+
+// Page Route
+app.get('/page/:page/:skip', routes.page);
+
+// Set /public as our static content dir
+app.use("/", express.static(__dirname + "/public/"));
+
+// Fire it up (start our server)
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
+// Initialize socket.io
+var io = require('socket.io').listen(server);
+
+// Set a stream listener for tweets matching tracking keywords
+twit.stream('statuses/filter',{ track: 'scotch_io, #scotchio'}, function(stream){
+  streamHandler(stream,io);
+});
